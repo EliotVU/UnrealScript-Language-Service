574d08396d5173a59428afd261891c31014d48c8
Eliot <eliot_van@hotmail.com>
2019-02-13 22:37:18 +0100

    Optimize LA().

--- uc/server/src/UC/CaseInsensitiveStream.ts
+++ uc/server/src/UC/CaseInsensitiveStream.ts
@@ -1,15 +1,22 @@
-import { ANTLRInputStream, IntStream } from 'antlr4ts';
+import { ANTLRInputStream } from 'antlr4ts';
 
 export class CaseInsensitiveStream extends ANTLRInputStream {
-	LA(i): number {
-		if (i === 0)
-			return 0;
-		if (i < 0)
-			++i;
-		var idx = this.p + i - 1;
-		if (idx >= this.n) {
-			return IntStream.EOF;
+	LA(i: number): number {
+		const c = super.LA(i);
+		if (c <= 0) {
+			return c;
 		}
-		return this.data[idx].toLowerCase().charCodeAt(0);
+		// return String.fromCodePoint(c).toLowerCase().charCodeAt(0);
+		return (c >= 65 && c <= 90) ? (97 + c - 65) : c;
+
+		// if (i === 0)
+		// 	return 0;
+		// if (i < 0)
+		// 	++i;
+		// var idx = this.p + i - 1;
+		// if (idx >= this.n) {
+		// 	return IntStream.EOF;
+		// }
+		// return this.data[idx].toLowerCase().charCodeAt(0);
 	}
 }
